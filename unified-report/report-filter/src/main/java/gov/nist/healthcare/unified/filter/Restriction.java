a28f0a9042719672f08531be4c942f9e2b8aa59d
Hossam Tamri <hossam.tamri@nist.gov>
2015-10-08 14:05:56 -0400

    Add getter/setter to restriction

--- unified-report/report-filter/src/main/java/gov/nist/healthcare/unified/filter/Restriction.java
+++ unified-report/report-filter/src/main/java/gov/nist/healthcare/unified/filter/Restriction.java
@@ -7,4 +7,12 @@ import java.util.ArrayList;
 public class Restriction {
 	public ArrayList<Condition> conditions = new ArrayList<Condition>();
 	public Action action;
+	
+	public void add(Condition c){
+		this.conditions.add(c);
+	}
+	
+	public void setAction(Action a){
+		this.action = a;
+	}
 }
