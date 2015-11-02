b6cd0f867a2924e1ade61b9391ef4139503d5677
Lefort, Maxence C <maxence.lefort@nist.gov>
2015-11-02 11:30:26 -0500

    Add XML Validation for ncpdp

---
+++ unified-report/validation-proxy/src/main/java/gov/nist/healthcare/unified/proxy/XMLValidationProxy.java
@@ -0,0 +1,44 @@
+package gov.nist.healthcare.unified.proxy;
+
+
+import java.util.List;
+
+import gov.nist.healthcare.unified.model.EnhancerH;
+import validator.Util;
+import validator.Validation;
+import gov.nist.healthcare.unified.enums.Context;
+import gov.nist.healthcare.unified.model.EnhancedReport;
+import gov.nist.healthcare.unified.model.Section;
+import gov.nist.validation.report.Report;
+
+import javax.xml.validation.Schema;
+
+public class XMLValidationProxy {
+
+    private Section service;
+
+    public XMLValidationProxy(String serName, String serProvider, String serVersion) {
+        service = new Section("service");
+        service.put("name", serName);
+        service.put("provider", serProvider);
+        service.put("version", serVersion);
+    }
+
+    public EnhancedReport validate(String msg, Schema schema, List<String> schematrons, String skeleton, String phase, Context context) {
+        try {
+            //String content = Util.streamAsString(msg);
+            Report validationReport = Validation.validate(msg, schema, schematrons, skeleton, phase);
+            String ctx = "";
+            if (context == Context.Free) ctx = "Context-Free";
+            else ctx = "Context-Based";
+            EnhancedReport enhancedReport = new EnhancedReport();
+            EnhancerH.enhanceHeader(enhancedReport, service, msg);
+            EnhancerH.enhanceWithReport(enhancedReport, validationReport);
+            return enhancedReport;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
